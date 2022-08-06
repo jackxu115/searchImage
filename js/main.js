7b3c917032144691237b64228dee94bdb7ef379a
pengxu19890115@gmail.com <pengxu19890115@gmail.com>
2022-08-05 23:33:03 -0700

    add comments

--- js/main.js
+++ js/main.js
@@ -1,34 +1,33 @@
+// data object
 const data = {
     access_key: 'Tos0NL02JwPPE1sWzetqCgyWjJLkuvzIut5bPRFXLkA',
     searchWord: null,
 }
 
-const fetchPhoto = () => {
+// get photo
+const cbFetch = () => {
     fetch(`https://api.unsplash.com/search/photos?client_id=${data.access_key}&page=1&query=${data.searchWord}`)
         .then(json => json.json())
         .then(img => {
-            console.log(img)
+            // console.log(img)
             const imgURL = img.results[0].urls.regular
             backgroundImage(imgURL)
         }).catch(error => console.log('this is error', error))
 }
 
+// update the search word
 const cbInput = event => {
     data.searchWord = event.target.value
 }
 
-
-const cbSearch = event => {
-    fetchPhoto()
-}
-
+// change background image
 const backgroundImage = (imgURL) => {
     document.body.style.backgroundImage = `url(${imgURL})`
     document.body.style.backgroundSize = 'cover'
 
 }
 
-
+// create header DOM
 const createHeader = () => {
     const eleHeader = document.createElement('header')
     const eleHeading = document.createElement('h1')
@@ -37,22 +36,31 @@ const createHeader = () => {
     return eleHeader
 }
 
+// create content DOM
 const createContent = () => {
     const eleContent = document.createElement('div')
     const eleInput = document.createElement('input')
     const eleButton = document.createElement('button')
     const eleIcon = document.createElement('i')
+
     eleContent.classList.add('content')
     eleIcon.classList.add('fa-solid', 'fa-magnifying-glass')
-    eleButton.appendChild(eleIcon)
+
     eleInput.addEventListener('input', cbInput)
-    eleButton.addEventListener('click', cbSearch)
+    eleButton.addEventListener('click', cbFetch)
+
+    eleButton.appendChild(eleIcon)
     eleContent.appendChild(eleInput)
     eleContent.appendChild(eleButton)
     return eleContent
 }
 
+// append DOM
+const appendDOM = () => {
+    document.body.appendChild(createHeader())
+    document.body.appendChild(createContent())
+}
 
-document.body.appendChild(createHeader())
-document.body.appendChild(createContent())
+appendDOM()
+
 
